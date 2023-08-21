9a6f241c9bd5aad65f4c089c5469ae1aadf0a680
manirDev <mhtnourmhtmjr@gmail.com>
2023-08-21 21:57:03 +0300

    area of a triangle.

---
+++ Area_Of_Right_Triangle/src/main/java/com/manirdev/Main.java
@@ -0,0 +1,23 @@
+package com.manirdev;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter first AB length: ");
+        a = scan.nextInt();
+        System.out.println("Enter first AC length: ");
+        b = scan.nextInt();
+        System.out.println("Enter first BC length: ");
+        c = scan.nextInt();
+
+        double surface = (double)(a + b + c)/2;
+        double area = Math.sqrt(surface*(surface -a )*(surface - b)*(surface - c));
+
+        System.out.println("Surface: " + surface);
+        System.out.println("Area: " + area);
+    }
+}
