6b844f6d710f02e790a13329d9f00895ea058803
manirDev <mhtnourmhtmjr@gmail.com>
2023-08-26 09:58:38 +0300

    grocery cashier updates.

--- GroceryCashier/src/main/java/com/manirdev/Main.java
+++ GroceryCashier/src/main/java/com/manirdev/Main.java
@@ -9,27 +9,20 @@ public class Main {
     static final Double apple = 3.67;
     static final Double tomato = 1.11;
     static final Double aubergine  = 2.14;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double totalKG = 0;
         double totalPrice = 0;
-
         System.out.print("Pear KG : ");
         double pearKG = scanner.nextDouble();
-
         System.out.print("Apple KG : ");
         double appleKG = scanner.nextDouble();
-
         System.out.print("Tomato KG : ");
         double tomatoKG = scanner.nextDouble();
-
         System.out.print("Aubergine KG : ");
         double aubergineKG = scanner.nextDouble();
-
         totalKG = pearKG + appleKG + tomatoKG + aubergineKG;
         totalPrice = pear*pearKG + apple*appleKG + tomato*tomatoKG + aubergine*aubergineKG;
-
         System.out.println("Total KG : " + totalKG);
         System.out.println("Total Price : " + totalPrice);
     }
